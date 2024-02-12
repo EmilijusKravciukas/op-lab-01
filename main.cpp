cb8e38bb818a98b2dc4cefdb970dc638395e7db6
EmilijusKravciukas <100075240+EmilijusKravciukas@users.noreply.github.com>
2024-02-12 20:19:50 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <iomanip>
 
 using namespace std;
 
@@ -15,15 +16,14 @@ int main(){
     ifstream DF("data.txt");
 
     int N;
-    DF>>N;
-    cout<<N;
+    DF >> N;
 
     Studentas studentai[N];
 
     for(int i = 0; i < N; i++){
-        DF>>studentai[i].vardas;
-        DF>>studentai[i].pavarde;
-        DF>>studentai[i].n;
+        DF >> studentai[i].vardas;
+        DF >> studentai[i].pavarde;
+        DF >> studentai[i].n;
         int temp = 0;
         for(int j = 0; j < studentai[i].n; j++){
             DF>>temp;
@@ -32,7 +32,14 @@ int main(){
         studentai[i].nd /= studentai[i].n;
         DF>>studentai[i].egz;
     }
-    
+
+    cout << left << setw(16) << "Pavardė" << left << setw(16) << "Vardas" << left << setw(16) << "Galutinis (Vid.)" << endl;
+    cout << "--------------------------------------------------------" << endl;
+    for(int i = 0; i < N; i++){
+        cout << left << setw(15) << studentai[i].vardas
+            << left << setw(16) << studentai[i].pavarde
+            << setprecision(2) << fixed << left << setw(16) << studentai[i].nd * 0.4 + double(studentai[i].egz) * 0.6 << endl;
+    }
 
     DF.close();
   
