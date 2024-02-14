b3791fc8852f2a81d062c425760af84b1a714128
EmilijusKravciukas <100075240+EmilijusKravciukas@users.noreply.github.com>
2024-02-14 09:13:21 +0200

    Update main.cpp
    Algorithm touch ups, code refactoring

--- main.cpp
+++ main.cpp
@@ -4,83 +4,91 @@
 
 using namespace std;
 
-int main(){
-    struct Studentas{
+struct Studentas{
         string vardas;
         string pavarde;
         int n;
+        double nd;
         int egz;
-        int nd[];
-    };
+        double mediana;
+};
+
+int m;
+
+void duomSkait(Studentas*& studentai);
+void atvaizd(Studentas*& studentai, int skaiciavimas);
+
+int main(){
+    int skaiciavimas = 0; //0 - vid, 1 - med
 
     ifstream DF("data.txt");
+    DF >> m;
+    DF.close();
 
-    int mInput;
-    char medianaUI = '0'; // 0 - rodo vidurki, 1 - rodo mediana
-    // cout << "Pasirinkite, rodyti pažymio vidurkį(0) ar mediana(1):";
-    // cin >> medianaUI;
-    DF >> mInput;
-    
-    const int m = mInput;
+    Studentas* studentai{new Studentas[m]{}};
 
-    Studentas studentai[m];
+
+    duomSkait(studentai);
+    atvaizd(studentai, skaiciavimas);
+
+
+    return 0;
+}
+
+
+void duomSkait(Studentas*& studentai){
+    ifstream DF("data.txt");
+
+    DF >> m;
 
     for(int i = 0; i < m; i++){
         DF >> studentai[i].vardas;
         DF >> studentai[i].pavarde;
         DF >> studentai[i].n;
         int temp = 0;
+        int* medianaMas{new int[studentai[i].n]{}};
         for(int j = 0; j < studentai[i].n; j++){
             DF>>temp;
-            studentai[i].nd[j] = temp;
+            studentai[i].nd += temp;
+            medianaMas[j] = temp;
+        }
+        studentai[i].nd /= studentai[i].n;
+        for(int j = 0; j < studentai[i].n-1; j++){
+            for(int z = 0; z < studentai[i].n; z++){
+                if(medianaMas[j] < medianaMas[z]){
+                    temp = medianaMas[j];
+                    medianaMas[j] = medianaMas[z];
+                    medianaMas[z] = temp;
+                }
+            }
+        }
+        if(studentai[i].n % 2 == 0){
+            studentai[i].mediana = (medianaMas[studentai[i].n/2] + medianaMas[studentai[i].n/2 + 1]) / 2;
+        } else {
+            studentai[i].mediana = medianaMas[studentai[i].n/2 + 1];
         }
         DF>>studentai[i].egz;
     }
 
-    if(medianaUI == '0'){
+    DF.close();
+}
+
+void atvaizd(Studentas*& studentai, int skaiciavimas){
+    if(skaiciavimas == 0) {
         cout << left << setw(16) << "Pavardė" << left << setw(16) << "Vardas" << left << setw(16) << "Galutinis (Vid.)" << endl;
         cout << "--------------------------------------------------------" << endl;
         for(int i = 0; i < m; i++){
-            double vid = 0;
-            for(int j = 0; j < studentai[i].n; j++){
-                vid += studentai[i].nd[j];
-            }
-            vid /= studentai[i].n;
-            
             cout << left << setw(15) << studentai[i].vardas
                 << left << setw(16) << studentai[i].pavarde
-                << setprecision(2) << fixed << left << setw(16) << vid * 0.4 + double(studentai[i].egz) * 0.6 << endl;
+                << setprecision(2) << fixed << left << setw(16) << studentai[i].nd * 0.4 + double(studentai[i].egz) * 0.6 << endl;
         }
     } else {
-        cout << left << setw(16) << "Pavardė" << left << setw(16) << "Vardas" << left << setw(16) << "Galutinis (Vid.)" << endl;
+        cout << left << setw(16) << "Pavardė" << left << setw(16) << "Vardas" << left << setw(16) << "Galutinis (Med.)" << endl;
         cout << "--------------------------------------------------------" << endl;
         for(int i = 0; i < m; i++){
-            double med = 0;
-            int temp = 0;
-            for(int j = 0; j < studentai[i].n-1; j++){
-                for(int z = j+1; z < studentai[i].n; z++){
-                    if(studentai[i].nd[j] < studentai[i].nd[z]){
-                        temp = studentai[i].nd[j];
-                        studentai[i].nd[j] = studentai[i].nd[z];
-                        studentai[i].nd[z] = temp;
-                    }
-                }
-            }
-            
-            if(m % 2 == 0){
-                cout << left << setw(15) << studentai[i].vardas
+            cout << left << setw(15) << studentai[i].vardas
                 << left << setw(16) << studentai[i].pavarde
-                << setprecision(2) << fixed << left << setw(16) << (studentai[i].nd[m/2] + studentai[i].nd[m/2 + 1]) / 2 * 0.4 + double(studentai[i].egz) * 0.6 << endl;
-            } else {
-                cout << left << setw(15) << studentai[i].vardas
-                << left << setw(16) << studentai[i].pavarde
-                << setprecision(2) << fixed << left << setw(16) << studentai[i].nd[m/2 + 1] * 0.4 + double(studentai[i].egz) * 0.6 << endl;
-            }
+                << setprecision(2) << fixed << left << setw(16) << double(studentai[i].mediana) * 0.4 + double(studentai[i].egz) * 0.6 << endl;
         }
     }
-    
-
-    DF.close();
-  
-    return 0;
 }
