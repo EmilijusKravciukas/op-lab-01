ae0296863ca95b5f2ebc79b3a0dd2c994e40aca3
EmilijusKravciukas <100075240+EmilijusKravciukas@users.noreply.github.com>
2024-02-23 12:15:24 +0200

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -59,27 +59,43 @@ void duomSkait(vector<Studentas>& studentai){
 
     ifstream DF(duomPav);
 
-    string temp;
-    int m = 0;
+    string tempString;
+    int tempInt;
+    unsigned int m = 0;
 
     do{
-        DF >> temp;
-    }while(temp != "Egzaminas");
+        DF >> tempString;
+    }while(tempString != "Egz.");
 
     while(DF.peek() != EOF){
-        while(DF.peek() != '\n'){
-            m++;
-            DF >> studentai[m].vardas;
-            DF >> studentai[m].pavarde;
-            
+        studentai.resize(m+1);
+        DF >> studentai[m].vardas;
+        DF >> studentai[m].pavarde;
+
+        int n = 0;
+
+        while(DF.peek() != '\n' && DF.peek() != EOF){
+            DF >> tempInt;
+            n++;
+            studentai[m].nd.push_back(tempInt);
         }
+
+        studentai[m].egz = studentai[m].nd[n-1];
+        studentai[m].nd.pop_back();
+        studentai[m].n = n;
+        studentai[m].vid = rastiVid(studentai, m);
+        studentai[m].mediana = rastiMed(studentai, m);
+
+        m++;
     }
+
+    atvaizd(studentai, m);
 }
 
 void ivestis(vector<Studentas>& studentai){
     bool loop = true;
-    int m = 0;
-    int n = 0;
+    unsigned int m = 0;
+    unsigned int n = 0;
     while(loop){
         cout << "Pasirinkite įvedimo būdą:" << endl
         << "(1) Duomenų įvedimas ranka" << endl
