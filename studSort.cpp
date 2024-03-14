e0036ee067a7d74d40509fc23290a94453788ad5
EmilijusKravciukas <100075240+EmilijusKravciukas@users.noreply.github.com>
2024-03-14 22:33:36 +0200

    Update studSort.cpp

--- studSort.cpp
+++ studSort.cpp
@@ -7,13 +7,15 @@ void studSort(vector<Studentas>& studentai, unsigned int m){
     sort(studentai.begin(), studentai.end(), rikiavimasPaz);
 
     for(Studentas studentas : studentai){
-        cout<<studentas.vardas<<" "<<studentas.pavarde<<" "<<(studentas.vid*0.4 + double(studentas.egz*0.6))<<endl;
+        cout<<studentas.vardas<<" "<<studentas.pavarde<<" "<<(studentas.vid*0.4 + double(studentas.egz) * 0.6)<<endl;
     }
 
     vector<Studentas> studentaiTemp;
 
+    const double epsilon = 1e-9;
+
     for(int i = 0; i < m; i++){
-        if((studentai[i].vid * 0.4 + double(studentai[i].egz * 0.6)) < 5){
+        if((studentai[i].vid * 0.4 + double(studentai[i].egz) * 0.6) < 5.0 - epsilon){
             for(int j = i; j < m; j++){
                 studentaiTemp.push_back(studentai[j]);
             }
@@ -29,12 +31,12 @@ void studSort(vector<Studentas>& studentai, unsigned int m){
             throw runtime_error(OPENING_ERROR);
         }
 
-        if(!(LF << left << setw(20) << "Vardas" << left << setw(20) << "Pavarde" << left << setw(20) << "Galutinis paz." << endl)){
+        if(!(LF << left << setw(20) << "Vardas" << left << setw(20) << "Pavarde" << left << setw(20) << "Galutinis paz.")){
             throw runtime_error(WRITING_ERROR);
         }
 
         for (Studentas studentas : studentaiTemp){
-            if(!(LF << left << setw(20) << studentas.vardas << left << setw(20) << studentas.pavarde << fixed << setprecision(2) << left << setw(20) << (studentas.vid * 0.4 + studentas.egz * 0.6) << endl)){
+            if(!(LF << endl << left << setw(20) << studentas.vardas << left << setw(20) << studentas.pavarde << fixed << setprecision(2) << left << setw(20) << (studentas.vid * 0.4 + double(studentas.egz) * 0.6))){
                 throw runtime_error(WRITING_ERROR);
             }
         }
@@ -49,12 +51,12 @@ void studSort(vector<Studentas>& studentai, unsigned int m){
             throw runtime_error(OPENING_ERROR);
         }
 
-        if(!(MF << left << setw(20) << "Vardas" << left << setw(20) << "Pavarde" << left << setw(20) << "Galutinis paz." << endl)){
+        if(!(MF << left << setw(20) << "Vardas" << left << setw(20) << "Pavarde" << left << setw(20) << "Galutinis paz.")){
             throw runtime_error(WRITING_ERROR);
         }
         
         for (Studentas studentas : studentai){
-            if(!(MF << left << setw(20) << studentas.vardas << left << setw(20) << studentas.pavarde << fixed << setprecision(2) << left << setw(20) << (studentas.vid * 0.4 + double(studentas.egz * 0.6)) << endl)){
+            if(!(MF << endl << left << setw(20) << studentas.vardas << left << setw(20) << studentas.pavarde << fixed << setprecision(2) << left << setw(20) << (studentas.vid * 0.4 + double(studentas.egz) * 0.6))){
                 throw runtime_error(WRITING_ERROR);
             }
         }
